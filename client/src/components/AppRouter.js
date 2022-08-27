332a58a90510ce5c4273c33473253bdedc065eb3
Azazlokus <azku@inbox.ru>
2022-08-27 22:49:44 +0700

    add routing

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,22 @@
+
+import React, {useContext} from 'react';
+import {Routes,Navigate, Route} from 'react-router-dom'
+import {authRoutes, publicRoutes} from "../routes";
+
+
+const AppRouter = () => {
+    const isAuth = false
+    return (
+        <Routes>
+            {isAuth && authRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component/>} exact/>
+            )}
+            {publicRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component/>} exact/>
+            )}
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+};
+
+export default AppRouter;
